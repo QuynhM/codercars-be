b470a3e693d2e5819ce78904c5cedb62834b2f91
Quynh Mai <quynhmai@Quynhs-MacBook-Pro.local>
2024-02-21 00:46:17 -0600

    Update files with answers

--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -1,37 +1,79 @@
 const mongoose = require('mongoose');
 const Car = require('../models/Car');
+const { AppError, sendResponse } = require('../helpers/utils');
 const carController = {};
 
 carController.createCar = async (req, res, next) => {
 	try {
-		// YOUR CODE HERE
+		const info = req.body;
+
+		if (!info) {
+			throw new AppError(400, "Bad Request", "Create Car Error");
+		} 
+
+		const created= await Car.create(info);
+		sendResponse(res,200,true,{Car:created},null,"Create Car Success");
+
+
 	} catch (err) {
-		// YOUR CODE HERE
+		next(err);
 	}
 };
 
 carController.getCars = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
+	const filter = {};
+
+	try{
+		//mongoose query
+		const listOfFound= await Car.find().limit(10).exec();
+		//this to query data from the reference and append to found result.
+		console.log("Car:",listOfFound)
+		sendResponse(res,200,true,{Car:listOfFound},null,"Found list of cars success");
+	
+	}catch(err){
+		next(err);
 	}
 };
 
 carController.editCar = async (req, res, next) => {
 	try {
-		// YOUR CODE HERE
+		const { id } = req.params;
+    	const updateInfo = req.body;
+
+		// Validate input
+		if (!updateInfo) {
+			throw new AppError(400, 'Bad Request', 'Update car Error');
+		  }
+		// mongoose query to find the car by ID and update it
+		const updatedCar = await Car.findByIdAndUpdate(id, updateInfo, {
+			new: true, // Return the modified document
+		});
+
+		if (!updatedCar) {
+			// If the car with the given ID is not found
+			throw new AppError(404, 'Not Found', 'Car not found');
+		}
+
+		// Send the response with the updated car data
+		sendResponse(res, 200, true, { Car: updatedCar }, null, 'Update Car Successfully');
 	} catch (err) {
-		// YOUR CODE HERE
+	next(err);
 	}
 };
 
 carController.deleteCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+	// empty target mean delete nothing
+    const targetId = null;
+    //options allow you to modify query. e.g new true return lastest update of data
+    const options = {new:true};
+    try{
+        //mongoose query
+        const updated= await Car.findByIdAndDelete(targetId,options)
+
+        sendResponse(res,200,true,{Car:updated},null,"Delete foo success")
+    }catch(err){
+        next(err)
+    }
 };
 
 module.exports = carController;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,29 @@
-const express = require('express');
-const router = express.Router();
+const { sendResponse, AppError}=require("../helpers/utils.js");
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.status(200).send("Welcome to CoderSchool!");
+});
+
+router.get("/template/:test", async(req,res,next)=>{
+    const { test } = req.params
+    try{
+        //turn on to test error handling
+        if(test==="error"){
+        throw new AppError(401,"Access denied","Authentication Error")
+        }else{
+        sendResponse(res,200,true,{data:"template"},null,"template success")
+        }
+    }catch(err){
+        next(err)
+    }
+})
 
 // CAR
-const carAPI = require('./car.api');
+const carAPI = require('./car.api.js');
 router.use('/car', carAPI);
 
 module.exports = router;
